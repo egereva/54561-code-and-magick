4d442630bbc1a71bac52288ac1c2e957c890dadf
Egereva Marina <marina.egereva@gmail.com>
2017-01-24 17:57:51 +0300

    Правки по замечаниям

--- js/stat.js
+++ js/stat.js
@@ -1,23 +1,39 @@
 'use strict';
 
-window.renderStatistics = function (ctx, names, times) {
+var drawCloud = function (ctx, x, y, width, height) {
   // рисуем тень
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillRect(x + 10, y + 10, width, height);
 
   // рисуем облако
   ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
-  ctx.fillRect(100, 10, 420, 270);
-  ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillRect(x, y, width, height);
+  ctx.strokeRect(x, y, width, height);
+};
+
+var writeText = function (ctx, text, x, y) {
+
+  var textArray = text.split('\n');
+  for (var i = 0; i < textArray.length; i++) {
+    ctx.fillStyle = '#000';
+    ctx.font = '16px PT Mono';
+    ctx.fillText(textArray[i], x, y + 20 * i);
+  }
+};
+
+var setColor = function (n1, n2) {
+  return ((Math.random() * n1) * n2).toFixed(0);
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  // рисуем облако
+  drawCloud(ctx, 100, 10, 420, 270);
 
   // выводим текст в облаке
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 130, 40);
-  ctx.fillText('Список результатов:', 130, 60);
+  writeText(ctx, 'Ура вы победили!\nСписок результатов:', 130, 40);
 
   // ищем наибольшее значение в массиве times
-  var max = -1; // почему -1? я взяла это из примера, но не поняла, почему не 0?
+  var max = -1;
 
   times.forEach(function (time) {
     if (time > max) {
@@ -25,20 +41,12 @@ window.renderStatistics = function (ctx, names, times) {
     }
   });
 
-  /* почему здесь не срабатывал этот код for (var i = 0; i < times.lenght; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
-    }
-  }?*/
-
-
   // рисуем гистограмму
-  var histoHeight = 150;
-  var histoWidth = 40;
-  var histoX = 150;
-  var step = histoHeight / max;
-  var columnIndent = 50;
+  var HISTO_HEIGHT = 150;
+  var HISTO_WIDTH = 40;
+  var HISTO_X = 150;
+  var step = HISTO_HEIGHT / max;
+  var COLUMN_INDENT = 50;
 
   for (var i = 0; i < times.length; i++) {
     var name = names[i];
@@ -47,21 +55,19 @@ window.renderStatistics = function (ctx, names, times) {
     var height = step * time;
 
     ctx.fillStyle = '#000';
-    ctx.fillText(time.toFixed(0), histoX + (histoWidth + columnIndent) * i, 90 + histoHeight - height);
+    ctx.fillText(time.toFixed(0), HISTO_X + (HISTO_WIDTH + COLUMN_INDENT) * i, 90 + HISTO_HEIGHT - height);
 
     if (name === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      var b = ((Math.random() * 5) * 50).toFixed(0);
+      var b = setColor(5, 50);
       var a = (Math.random()).toFixed(1);
-      ctx.fillStyle = 'rgba(0, 0, ' + b + ' , ' + a + ')';  // не очень поняла момент написания '+ b +' и '+ a +' . нашла пример в интернете и использовала его.
+      ctx.fillStyle = 'rgba(0, 0, ' + b + ' , ' + a + ')';
     }
-    ctx.fillRect(histoX + (histoWidth + columnIndent) * i, 100 + histoHeight - height, histoWidth, height);
+    ctx.fillRect(HISTO_X + (HISTO_WIDTH + COLUMN_INDENT) * i, 100 + HISTO_HEIGHT - height, HISTO_WIDTH, height);
 
     ctx.fillStyle = '#000';
-    ctx.fillText(name, histoX + (histoWidth + columnIndent) * i, 120 + histoHeight);
+    ctx.fillText(name, HISTO_X + (HISTO_WIDTH + COLUMN_INDENT) * i, 120 + HISTO_HEIGHT);
   }
 
 };
-
-var canvas = document.querySelector('canvas');
